0840b29c0e721958f17c64aab4246c8baeade56c
JustineDevs <TraderGOfficial@gmail.com>
2026-03-08 15:07:09 +0800

    update: modify index.ts in ui

--- apps/studio/components/ui/index.ts
+++ apps/studio/components/ui/index.ts
@@ -3,3 +3,6 @@ export { MetricCard } from "./MetricCard";
 export { EmptyState } from "./EmptyState";
 export { StatusBadge, type StatusBadgeVariant } from "./StatusBadge";
 export { DataTable } from "./DataTable";
+export { LiveIndicator } from "./LiveIndicator";
+export { BentoGrid, BentoCard } from "./BentoGrid";
+export { NetworkTopologyMap } from "./NetworkTopologyMap";

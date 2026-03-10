08ad617c2e66763e95912593659aa547953edfd8
JustineDevs <TraderGOfficial@gmail.com>
2026-03-10 13:42:51 +0800

    update: modify index.ts in web3-utils

--- packages/web3-utils/index.ts
+++ packages/web3-utils/index.ts
@@ -1,6 +1,7 @@
 /**
  * @hyperagent/web3-utils
  * Shared Web3/IPFS/deploy capability toolkits. Services use these instead of raw vendor SDKs.
+ * Hybrid chain model: thirdweb for chain metadata/execution, HyperAgent capabilities for policy.
  */
 export { IpfsPinataToolkit } from "./ipfs-pinata.js";
 export type { IpfsPinataToolkitOptions } from "./ipfs-pinata.js";
@@ -11,3 +12,17 @@ export type {
   ChainRegistryMap,
   RegistryLoader,
 } from "./deploy-toolkit.js";
+export {
+  resolveNetwork,
+  resolveNetworkByChainId,
+  loadCapabilities,
+  initCapabilities,
+  buildChainRegistryFromCapabilities,
+  getCachedCapabilities,
+  loadHybridChainRegistry,
+} from "./resolve-network.js";
+export type {
+  ChainCapabilities,
+  CapabilitiesMap,
+  ResolvedNetwork,
+} from "./resolve-network.js";

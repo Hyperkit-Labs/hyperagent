f929bc8670e1daa4a9a5241651cbfd0ef79d539e
JustineDevs <TraderGOfficial@gmail.com>
2026-04-20 21:14:19 +0800

    feat: add infra/terraform/modules/kms/main.tf

---
+++ infra/terraform/modules/kms/main.tf
@@ -0,0 +1,6 @@
+# Stub: replace with aws_kms_key / google_kms_crypto_key for your provider.
+# Set output key_arn as LLM_KEY_KMS_KEY_ARN in orchestrator secrets.
+
+terraform {
+  required_version = ">= 1.5.0"
+}
